ef6377f246e8a00238a6d99f90a3e9c70dff338d
大城小爱 <39391600+WuChengqian520@users.noreply.github.com>
2019-05-14 21:27:51 +0800

    Update stag.py
    修改测试数据库

--- config/stag.py
+++ config/stag.py
@@ -18,12 +18,12 @@ RUN_MODE = 'STAGING'
 DATABASES.update(
     {
         'default': {
-            'ENGINE': '',
-            'NAME': '',  # 数据库名
-            'USER': '',  # 数据库用户
-            'PASSWORD': '',  # 数据库密码
-            'HOST': '',  # 数据库主机
-            'PORT': '',  # 数据库端口
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'hello',  # 数据库名
+            'USER': 'root',  # 数据库用户
+            'PASSWORD': 'hello',  # 数据库密码
+            'HOST': 'localhost',  # 数据库主机
+            'PORT': '3306',  # 数据库端口
         },
     }
 )
