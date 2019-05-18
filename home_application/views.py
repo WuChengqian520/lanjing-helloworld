a0b6eb94f7d57678f1bf49b4d9e21aa984176d0b
wu <406615373@qq.com>
2019-05-18 14:26:54 +0800

    完成第二次作业

--- home_application/views.py
+++ home_application/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from blueapps.account.decorators import login_exempt
-
+from django.http import JsonResponse
 
 # 开发框架中通过中间件默认是需要登录态的，如有不需要登录的，可添加装饰器login_exempt
 # 装饰器引入 from blueapps.account.decorators import login_exempt
@@ -12,3 +12,18 @@ def home(request):
     首页
     """
     return render(request, 'home_application/helloworld.html')
+
+
+@login_exempt
+def second(request):
+    """
+    第二次作业：判断，返回
+    """
+    if request.method == "GET":
+        return render(request, 'home_application/inpnut_output.html')
+    elif request.method == "POST":
+        input_value = request.POST.get('input2').strip()
+        if input_value == "Hello Blueking":
+            return JsonResponse({'code': 200})
+        else:
+            return JsonResponse({"code": 400})
